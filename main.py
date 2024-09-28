e60705ed62394826b22cfe49c561b446804f97b0
subhash0927 <subhashdas927@gmail.com>
2024-09-29 01:44:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import streamlit as st
 st.title('BMI Calculator')
 st.subheader('This application is designed to calculate BMI')
 name = st.text_input('Name',placeholder ='Enter your name')
+if(name!=''):
+    st.text(f'Hello{initial}{name},Please enter appropriate fields')
+
 gender = st.radio('Gender',('Male','Female'))
 if(gender=='Male'):
     initial = 'Mr'
 else:
     initial = 'Mrs'
-if(name!=''):
-    st.text(f'Hello{initial}{name},Please enter appropriate fields')
 
 
 weight = st.slider('Range of weight',min_value =0,max_value=250)
